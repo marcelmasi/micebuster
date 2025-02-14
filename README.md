65ad667aced7a75a2ff045f5ae8c83c57d3c400d
Marcel Germann <bitbucket@masi.li>
2025-02-14 10:49:17 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# micebuster
-Use audio to get rid of mice and rats
+# Micebuster
+To get rid of mice and rats without killing them.
+
+## Background
+Mice ant rats can hear ultrasound (e.g. sound above a frequency of 20000Hz), but humans
+can not. Apparently, mice and rats do not like it if you play randomly pitched sound
+pulses between 22kHz and 35hHz. So you actually can use audio to get rid of them. If
+you use this over days or weeks, they will simply go away and find another place to live.
+
+However, since mice may become accustomed to a single frequency over time, the frequency
+should be varied.
+
+This application is designed exactly for that: randomly pitched sound at high frequencies.
+
+## Warning
+**Never use this near dogs, cats or any other pets. Also, there is research showing ultrasound
+frequencies might sometimes still be sensed by humans or could cause headaches.**
+
+**Never use too loud sounds, especially in the frequencies of the human ear. You might cause damage to the ear.**
+
+## Requirements
+### Hardware
+You need a speaker that has enough power (dB) also at a range between 22kHz and 35kHz.
+I use a HomePod Mini for this, although it seems to have quite a falloff already at 17-20kHz (
+https://www.rtings.com/speaker/reviews/apple/homepod-mini ), so I use as default a frequency between 15kHz and 20kHz.
+
+## Installation
+- If you do not have Conda already, get it from, e.g., here: https://docs.anaconda.com/free/anaconda/install/index.html
+- On Mac you need to do: `brew install portaudio`
+- `conda env create -f environment.yml`
+
+## Run
+- `conda activate micebuster`
+- `python audio_generator.py`
+
+If you want to store the generated audio wave in a file, just add the flag `--save [filename]` where you replace
+`[filename]` with the target wave file, e.g. `random_15kHz_to_20kHz.wav`.
+
+You can also define the base frequency (`--low [frequency Hz]`) and the range (`--high [frequency Hz]`), where the
+range between low and high define the frequency range for the uniformly generated random frequencies.
---
+++ micebuster.py
@@ -0,0 +1,94 @@
+import argparse
+import random
+from typing import Optional
+
+import numpy as np
+import sounddevice as sd
+
+
+def generate_sine_wave(frequency, duration, sample_rate=44100) -> np.ndarray:
+    """
+    Generate a sine wave of a given frequency and duration.
+
+    Args:
+        frequency   : Frequency of the sine wave in Hertz.
+        duration    : Duration of the wave in seconds.
+        sample_rate : Sample rate in samples per second.
+
+    Returns:
+        A numpy array containing the sine wave.
+    """
+    t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
+    wave = 0.5 * np.sin(2 * np.pi * frequency * t)
+    return wave
+
+
+def play_wave(wave: np.ndarray, sample_rate=44100) -> None:
+    """
+    Play a given wave using sounddevice.
+
+    Args:
+        wave        : A numpy array containing the wave data.
+        sample_rate : Sample rate in samples per second.
+    """
+    sd.play(wave, samplerate=sample_rate)
+    sd.wait()  # Wait until the sound has finished playing
+
+
+def write_wave_to_file(wave: np.ndarray, file_name: str, sample_rate=44100) -> None:
+    import scipy.io.wavfile
+    scipy.io.wavfile.write(file_name, sample_rate, wave)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--low",
+        type=int,
+        default=15000,
+        help="Lower bound of the uniformly generated random frequencies in Hz"
+    )
+    parser.add_argument(
+        "--high",
+        type=int,
+        default=20000,
+        help="Upper bound of the uniformly generated random frequencies in Hz"
+    )
+    parser.add_argument(
+        "--number",
+        type=int,
+        default=10,
+        help="How many frequencies to generate"
+    )
+    parser.add_argument(
+        "--duration",
+        type=int,
+        default=5,
+        help="Duration in seconds of the sound wave per frequency. The overall length of sounds is number * duration."
+    )
+    parser.add_argument(
+        "--save",
+        type=str,
+        default=None,
+        help="File name to write the audio instead of playing it. This will create a WAV file, so you should use the "
+             ".wav extension."
+    )
+    args = parser.parse_args()
+
+    all_wave_data: Optional[np.ndarray] = None
+
+    # Generate sine wave
+    for i in range(args.number):
+        random_frequency = random.randint(args.low, args.high)
+        one_frequency_wave = generate_sine_wave(frequency=random_frequency, duration=float(args.duration))
+        all_wave_data = \
+            np.concatenate([all_wave_data, one_frequency_wave]) if all_wave_data is not None else one_frequency_wave
+
+    if args.save:
+        write_wave_to_file(wave=all_wave_data, file_name=args.save)
+    else:
+        play_wave(all_wave_data)
+
+
+if __name__ == "__main__":
+    main()
